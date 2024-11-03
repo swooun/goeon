a2e235e11d6e69034f31986c6174f5d8e8e36575
swooun <go2_on@kakao.com>
2024-11-03 18:17:27 +0900

    week09

---
+++ week09/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int all_files;
+
+extern void sub();
+
+int main(void)
+{
+	sub();
+	printf("%d\n", all_files);
+	return 0;
+}
+
+extern int all_files;
+void sub(void)
+{
+	all_files = 10;
+}
---
+++ week09/week09_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main(void) {
+	
+	int flag = 1;
+	while(flag != 0)
+	{
+		int y;
+		y = 3;
+		flag = 0;
+	}
+	y=4;
+}*/
+
+int main(void) {
+	
+	int flag = 1;
+	int y; /* y가 정의되지 않아 윗 줄에 int y를 추가함*/ 
+	while(flag != 0)
+	{
+		int y;
+		y = 3;
+		flag = 0;
+	}
+	y=4;
+}
+

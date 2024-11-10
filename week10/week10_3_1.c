05329b8fa41ce26ff3dfb75cec7b02d7b6435c42
swooun <go2_on@kakao.com>
2024-11-10 19:53:10 +0900

    week10

---
+++ week10/week10_3_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+
+int main(void)
+{
+	int i;
+	int grade[SIZE];
+	int score[SIZE];
+	
+	for (i=0;i<SIZE;i++)
+	    grade[i] = rand() % 100;
+	    score[i] = grade[i];
+	    
+	for (i=0;i<SIZE;i++)
+	    printf("score[%d] = %d\n", i, score[i]);
+	    
+	return 0;
+}
